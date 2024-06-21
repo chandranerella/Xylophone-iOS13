f54eb85919be140373c12f91821958012de0294f
Chandra Sekhar Nerella <chandrasekharnerella@Chandras-MBP.lan>
2024-06-21 15:08:49 -0400

    Complete Xylophone app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var bombSoundEffect: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func aKeyPress(_ sender: UIButton) {
+        let fileName = sender.currentTitle ?? "C" + ".wav"
+        playNote(file: fileName)
+    }
+    
+    func playNote(file: String) {
+        let path = Bundle.main.path(forResource: file + ".wav", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }
     
 
 }
